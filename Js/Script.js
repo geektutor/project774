b7c1c160a4ccc635de0b23d5b9bddaa9c8e31897
Favourene <igbinosaiwinosa01@gmail.com>
2022-02-08 13:44:11 +0100

    Home Page done

--- Js/Script.js
+++ Js/Script.js
@@ -8,3 +8,34 @@ toggle.onclick = function () {
 
   nav.classList.toggle('collapse')
 }
+
+var swiper = new Swiper('.mySwiper', {
+  slidesPerView: 3,
+  spaceBetween: 10,
+  loop: true,
+  coverflowEffect: {
+    rotate: 0,
+    stretch: 0,
+    depth: 100,
+    modifier: 1,
+    slideShadows: true,
+  },
+  autoplay: {
+    delay: 1500,
+    disableOnInteraction: false,
+  },
+  breakpoints: {
+    1025: {
+      slidesPerView: 3,
+      spaceBetween: 10,
+    },
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 10,
+    },
+    0: {
+      slidesPerView: 1,
+      spaceBetween: 10,
+    },
+  },
+})
